82bc575734010b8d9fd7f1a54cef43fbd7ff28be
(neha ali) <159723021+nehali123@users.noreply.github.com>
2024-03-01 11:58:54 +0500

    first commit

---
+++ 45 assignmnt Q#7/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//storing a mathematical operations
+//print the value of 8 by addition 
+const addition = 2 + 6;
+console.log(addition);
+//print the value of 8 by substraction
+const substraction = 10 - 2;
+console.log(substraction);
+//print the value of 8 by multiplication
+const multiplication = 2 * 4;
+console.log(multiplication);
+//print the value of 8 by division
+let division = 16 / 2;
+console.log(division);
