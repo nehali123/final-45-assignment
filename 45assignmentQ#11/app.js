4a3508bca0b03009f967fe3ad92d068db5ce6f11
“[firstname <[valid-email]>
2024-03-17 22:41:41 +0500

    third commit

---
+++ 45assignmentQ#11/app.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Store the names of a few of your friends in an array called names. Print each person’s name by
+accessing each element in the list, one at a time*/
+let Names = ["sidra", "anum", "roman", "sana"];
+console.log(Names[0]);
+console.log(Names[1]);
+console.log(Names[2]);
+console.log(Names[3]);
---
+++ 45assignmentQ#11/app.ts
@@ -0,0 +1,15 @@
+/*Store the names of a few of your friends in an array called names. Print each person’s name by 
+accessing each element in the list, one at a time*/
+
+let Names : string[] = ["sidra","anum","roman" , "sana"];
+
+console.log(Names[0]);
+
+console.log(Names[1]);
+
+console.log(Names[2]);
+
+console.log(Names[3]);
+
+
+
