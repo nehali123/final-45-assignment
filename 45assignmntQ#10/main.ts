297730ba80c083dae870c50d9aa8ca72c43263a0
“[firstname <[valid-email]>
2024-03-17 22:09:58 +0500

    second commit

---
+++ 45assignmntQ#10/main.ts
@@ -0,0 +1,13 @@
+/*Choose two of the programs you’ve written, and add at least one comment to each. If you don’t 
+have anything specific to write because your programs are too simple at this point, just add your name 
+and the current date at the top of each program file. Then write one sentence describing what the 
+program does.*/
+
+// author : [neha ali]
+// date : [friday , march , 4 , 2024]
+
+//task # 9 printing my favourite number :
+let favnum : number = 7;
+
+//reveling my favourite number in a message formate
+console.log(`my favourite number is ${favnum}`)
