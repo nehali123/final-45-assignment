25784d21a2d053b8b5aa15361315e3262249f46e
“[firstname <[valid-email]>
2024-03-17 07:32:23 +0500

    second commit

--- 45 assignmt Q#2/main.js
+++ 45 assignmt Q#2/main.js
@@ -1,7 +1,17 @@
 "use strict";
 //store a personname in a variable
 let personname = "nazish ali";
-//printing a personname in lowercase
+// lowercase 
 console.log("lowercase:", personname.toLowerCase());
+//UPPERCASE
 console.log("uppercase:", personname.toUpperCase());
-console.log("titlecase:", personname.toTitle_Case());
+//TITLEcase 
+//step 1
+let words = personname.split(" ");
+//step 2
+let titlecaseName = " ";
+//step 3
+for (let i = 0; i < words.length; i++) {
+    titlecaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
+}
+console.log("Titlecase:", titlecaseName);
--- 45 assignmt Q#2/main.ts
+++ 45 assignmt Q#2/main.ts
@@ -3,8 +3,24 @@
 let personname :any= "nazish ali";
 
 // lowercase 
+
 console.log("lowercase:",personname.toLowerCase())
+
 //UPPERCASE
+
 console.log("uppercase:",personname.toUpperCase())
-//TITLEcase
-console.log("titlecase:",personname.toTitlecase())
+
+//TITLEcase 
+//step 1
+let words: string[]=personname.split(" ");
+
+//step 2
+let titlecaseName:string= " "
+//step 3
+ 
+for (let i= 0; i < words.length; i++)  {
+    titlecaseName +=words[i].charAt(0).toUpperCase()+words[i].slice(1).toLowerCase() +" ";
+    
+}
+
+console.log("Titlecase:" ,titlecaseName)
