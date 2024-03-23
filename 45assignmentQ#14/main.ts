44ed73e5bb25826b94f3a222e05cde11fead729b
“[firstname <[valid-email]>
2024-03-24 00:13:16 +0500

    second commit

--- 45assignmentQ#14/main.ts
+++ 45assignmentQ#14/main.ts
@@ -3,6 +3,7 @@
 includes at least three people you’d like to invite to dinner. Then use your list to print a message to 
 each person, inviting them to dinner*/
 
+
 let guest_list: string[]= ["ali","neha","hina"]
 for (let i = 0; i < guest_list.length; i++) {
     console.log('Respected Sir/Madam'+ guest_list[i] + ',\nwe invited you on dinner tomorrow.\n\nThank You');
